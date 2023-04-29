1d55c30dd682e90fa9e106acaea2a7bc6f7dfa8a
Tony Froman <33915703+fromanan@users.noreply.github.com>
2023-04-29 00:17:13 -0400

    Updated FrameBasicTextbox/FrameBasicDropdown to not cut off the accept/cancel buttons

--- src/JackTheVideoRipper/views/modals/FrameBasicDropdown.Designer.cs
+++ src/JackTheVideoRipper/views/modals/FrameBasicDropdown.Designer.cs
@@ -28,73 +28,70 @@
       /// </summary>
       private void InitializeComponent()
       {
-            this.comboBox = new System.Windows.Forms.ComboBox();
-            this.MainLabel = new System.Windows.Forms.Label();
-            this.buttonCancel = new System.Windows.Forms.Button();
-            this.buttonConfirm = new System.Windows.Forms.Button();
-            this.SuspendLayout();
-            // 
-            // comboBox
-            // 
-            this.comboBox.FormattingEnabled = true;
-            this.comboBox.Location = new System.Drawing.Point(10, 36);
-            this.comboBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.comboBox.Name = "comboBox";
-            this.comboBox.Size = new System.Drawing.Size(162, 23);
-            this.comboBox.TabIndex = 0;
-            // 
-            // MainLabel
-            // 
-            this.MainLabel.BackColor = System.Drawing.SystemColors.ControlLight;
-            this.MainLabel.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.MainLabel.Location = new System.Drawing.Point(10, 7);
-            this.MainLabel.Name = "MainLabel";
-            this.MainLabel.Padding = new System.Windows.Forms.Padding(3);
-            this.MainLabel.Size = new System.Drawing.Size(162, 24);
-            this.MainLabel.TabIndex = 14;
-            this.MainLabel.Text = "Please select a value";
-            // 
-            // buttonCancel
-            // 
-            this.buttonCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.buttonCancel.Location = new System.Drawing.Point(10, 62);
-            this.buttonCancel.Margin = new System.Windows.Forms.Padding(2);
-            this.buttonCancel.Name = "buttonCancel";
-            this.buttonCancel.Size = new System.Drawing.Size(80, 28);
-            this.buttonCancel.TabIndex = 15;
-            this.buttonCancel.Text = "Cancel";
-            this.buttonCancel.UseVisualStyleBackColor = true;
-            // 
-            // buttonConfirm
-            // 
-            this.buttonConfirm.Location = new System.Drawing.Point(93, 62);
-            this.buttonConfirm.Margin = new System.Windows.Forms.Padding(2);
-            this.buttonConfirm.Name = "buttonConfirm";
-            this.buttonConfirm.Size = new System.Drawing.Size(80, 28);
-            this.buttonConfirm.TabIndex = 16;
-            this.buttonConfirm.Text = "Confirm";
-            this.buttonConfirm.UseVisualStyleBackColor = true;
-            // 
-            // FrameBasicDropdown
-            // 
-            this.AcceptButton = this.buttonConfirm;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.CancelButton = this.buttonCancel;
-            this.ClientSize = new System.Drawing.Size(184, 101);
-            this.ControlBox = false;
-            this.Controls.Add(this.buttonConfirm);
-            this.Controls.Add(this.buttonCancel);
-            this.Controls.Add(this.MainLabel);
-            this.Controls.Add(this.comboBox);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.Name = "FrameBasicDropdown";
-            this.ShowIcon = false;
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
-            this.Text = "BasicDropdown";
-            this.TopMost = true;
-            this.ResumeLayout(false);
+         this.comboBox = new System.Windows.Forms.ComboBox();
+         this.MainLabel = new System.Windows.Forms.Label();
+         this.buttonCancel = new System.Windows.Forms.Button();
+         this.buttonConfirm = new System.Windows.Forms.Button();
+         this.SuspendLayout();
+         // 
+         // comboBox
+         // 
+         this.comboBox.FormattingEnabled = true;
+         this.comboBox.Location = new System.Drawing.Point(11, 48);
+         this.comboBox.Name = "comboBox";
+         this.comboBox.Size = new System.Drawing.Size(185, 28);
+         this.comboBox.TabIndex = 0;
+         // 
+         // MainLabel
+         // 
+         this.MainLabel.BackColor = System.Drawing.SystemColors.ControlLight;
+         this.MainLabel.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+         this.MainLabel.Location = new System.Drawing.Point(11, 9);
+         this.MainLabel.Name = "MainLabel";
+         this.MainLabel.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
+         this.MainLabel.Size = new System.Drawing.Size(185, 32);
+         this.MainLabel.TabIndex = 14;
+         this.MainLabel.Text = "Please select a value";
+         // 
+         // buttonCancel
+         // 
+         this.buttonCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+         this.buttonCancel.Location = new System.Drawing.Point(11, 83);
+         this.buttonCancel.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+         this.buttonCancel.Name = "buttonCancel";
+         this.buttonCancel.Size = new System.Drawing.Size(91, 37);
+         this.buttonCancel.TabIndex = 15;
+         this.buttonCancel.Text = "Cancel";
+         this.buttonCancel.UseVisualStyleBackColor = true;
+         // 
+         // buttonConfirm
+         // 
+         this.buttonConfirm.Location = new System.Drawing.Point(106, 83);
+         this.buttonConfirm.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+         this.buttonConfirm.Name = "buttonConfirm";
+         this.buttonConfirm.Size = new System.Drawing.Size(91, 37);
+         this.buttonConfirm.TabIndex = 16;
+         this.buttonConfirm.Text = "Confirm";
+         this.buttonConfirm.UseVisualStyleBackColor = true;
+         // 
+         // FrameBasicDropdown
+         // 
+         this.AcceptButton = this.buttonConfirm;
+         this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+         this.CancelButton = this.buttonCancel;
+         this.ClientSize = new System.Drawing.Size(210, 135);
+         this.ControlBox = false;
+         this.Controls.Add(this.buttonConfirm);
+         this.Controls.Add(this.buttonCancel);
+         this.Controls.Add(this.MainLabel);
+         this.Controls.Add(this.comboBox);
+         this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+         this.Name = "FrameBasicDropdown";
+         this.ShowIcon = false;
+         this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+         this.Text = "BasicDropdown";
+         this.TopMost = true;
+         this.ResumeLayout(false);
 
       }
 
--- src/JackTheVideoRipper/views/modals/FrameBasicTextbox.Designer.cs
+++ src/JackTheVideoRipper/views/modals/FrameBasicTextbox.Designer.cs
@@ -28,75 +28,72 @@
       /// </summary>
       private void InitializeComponent()
       {
-            this.MainLabel = new System.Windows.Forms.Label();
-            this.buttonCancel = new System.Windows.Forms.Button();
-            this.buttonConfirm = new System.Windows.Forms.Button();
-            this.textBox = new System.Windows.Forms.TextBox();
-            this.SuspendLayout();
-            // 
-            // MainLabel
-            // 
-            this.MainLabel.BackColor = System.Drawing.SystemColors.ControlLight;
-            this.MainLabel.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.MainLabel.Location = new System.Drawing.Point(10, 7);
-            this.MainLabel.Name = "MainLabel";
-            this.MainLabel.Padding = new System.Windows.Forms.Padding(3);
-            this.MainLabel.Size = new System.Drawing.Size(162, 24);
-            this.MainLabel.TabIndex = 14;
-            this.MainLabel.Text = "Please enter a value";
-            // 
-            // buttonCancel
-            // 
-            this.buttonCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.buttonCancel.Location = new System.Drawing.Point(10, 62);
-            this.buttonCancel.Margin = new System.Windows.Forms.Padding(2);
-            this.buttonCancel.Name = "buttonCancel";
-            this.buttonCancel.Size = new System.Drawing.Size(80, 28);
-            this.buttonCancel.TabIndex = 15;
-            this.buttonCancel.Text = "Cancel";
-            this.buttonCancel.UseVisualStyleBackColor = true;
-            // 
-            // buttonConfirm
-            // 
-            this.buttonConfirm.Location = new System.Drawing.Point(93, 62);
-            this.buttonConfirm.Margin = new System.Windows.Forms.Padding(2);
-            this.buttonConfirm.Name = "buttonConfirm";
-            this.buttonConfirm.Size = new System.Drawing.Size(80, 28);
-            this.buttonConfirm.TabIndex = 16;
-            this.buttonConfirm.Text = "Confirm";
-            this.buttonConfirm.UseVisualStyleBackColor = true;
-            // 
-            // textBox
-            // 
-            this.textBox.Location = new System.Drawing.Point(10, 36);
-            this.textBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.textBox.Name = "textBox";
-            this.textBox.Size = new System.Drawing.Size(162, 23);
-            this.textBox.TabIndex = 17;
-            // 
-            // FrameBasicTextbox
-            // 
-            this.AcceptButton = this.buttonConfirm;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.CancelButton = this.buttonCancel;
-            this.ClientSize = new System.Drawing.Size(184, 101);
-            this.ControlBox = false;
-            this.Controls.Add(this.textBox);
-            this.Controls.Add(this.buttonConfirm);
-            this.Controls.Add(this.buttonCancel);
-            this.Controls.Add(this.MainLabel);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.MaximizeBox = false;
-            this.MinimizeBox = false;
-            this.Name = "FrameBasicTextbox";
-            this.ShowIcon = false;
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
-            this.Text = "BasicTextbox";
-            this.TopMost = true;
-            this.ResumeLayout(false);
-            this.PerformLayout();
+         this.MainLabel = new System.Windows.Forms.Label();
+         this.buttonCancel = new System.Windows.Forms.Button();
+         this.buttonConfirm = new System.Windows.Forms.Button();
+         this.textBox = new System.Windows.Forms.TextBox();
+         this.SuspendLayout();
+         // 
+         // MainLabel
+         // 
+         this.MainLabel.BackColor = System.Drawing.SystemColors.ControlLight;
+         this.MainLabel.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+         this.MainLabel.Location = new System.Drawing.Point(11, 9);
+         this.MainLabel.Name = "MainLabel";
+         this.MainLabel.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
+         this.MainLabel.Size = new System.Drawing.Size(185, 32);
+         this.MainLabel.TabIndex = 14;
+         this.MainLabel.Text = "Please enter a value";
+         // 
+         // buttonCancel
+         // 
+         this.buttonCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+         this.buttonCancel.Location = new System.Drawing.Point(11, 83);
+         this.buttonCancel.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+         this.buttonCancel.Name = "buttonCancel";
+         this.buttonCancel.Size = new System.Drawing.Size(91, 37);
+         this.buttonCancel.TabIndex = 15;
+         this.buttonCancel.Text = "Cancel";
+         this.buttonCancel.UseVisualStyleBackColor = true;
+         // 
+         // buttonConfirm
+         // 
+         this.buttonConfirm.Location = new System.Drawing.Point(106, 83);
+         this.buttonConfirm.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+         this.buttonConfirm.Name = "buttonConfirm";
+         this.buttonConfirm.Size = new System.Drawing.Size(91, 37);
+         this.buttonConfirm.TabIndex = 16;
+         this.buttonConfirm.Text = "Confirm";
+         this.buttonConfirm.UseVisualStyleBackColor = true;
+         // 
+         // textBox
+         // 
+         this.textBox.Location = new System.Drawing.Point(11, 48);
+         this.textBox.Name = "textBox";
+         this.textBox.Size = new System.Drawing.Size(185, 27);
+         this.textBox.TabIndex = 17;
+         // 
+         // FrameBasicTextbox
+         // 
+         this.AcceptButton = this.buttonConfirm;
+         this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+         this.CancelButton = this.buttonCancel;
+         this.ClientSize = new System.Drawing.Size(210, 135);
+         this.ControlBox = false;
+         this.Controls.Add(this.textBox);
+         this.Controls.Add(this.buttonConfirm);
+         this.Controls.Add(this.buttonCancel);
+         this.Controls.Add(this.MainLabel);
+         this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+         this.MaximizeBox = false;
+         this.MinimizeBox = false;
+         this.Name = "FrameBasicTextbox";
+         this.ShowIcon = false;
+         this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+         this.Text = "BasicTextbox";
+         this.TopMost = true;
+         this.ResumeLayout(false);
+         this.PerformLayout();
 
       }
 
