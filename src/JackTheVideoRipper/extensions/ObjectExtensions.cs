e889f21df39d0209b107d38da3adee62afcb05eb
Tony Froman <33915703+fromanan@users.noreply.github.com>
2023-04-29 02:50:53 -0400

    Added Is<T> extension for objects

--- src/JackTheVideoRipper/extensions/ObjectExtensions.cs
+++ src/JackTheVideoRipper/extensions/ObjectExtensions.cs
@@ -4,16 +4,18 @@ public static class ObjectExtensions
 {
     public static T As<T>(this object obj)
     {
-        var objType = obj.GetType();
-
-        if (!objType.IsAssignableTo(typeof(T)) && 
-            !objType.IsEquivalentTo(typeof(T)) && 
-            !objType.IsInstanceOfType(typeof(T)))
-        {
+        if (!obj.Is<T>())
             throw new InvalidCastException();
-        }
 
         return (T)obj;
+    }
+
+    public static bool Is<T>(this object obj)
+    {
+        Type objType = obj.GetType();
+        return objType.IsAssignableTo(typeof(T)) ||
+               objType.IsEquivalentTo(typeof(T)) || 
+               objType.IsInstanceOfType(typeof(T));
     }
 
     public static T Cast<T>(this object obj)
