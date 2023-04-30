01ca208fc2e7fd7b5311691061a306188f53aff6
Tony Froman <33915703+fromanan@users.noreply.github.com>
2023-04-30 00:11:06 -0400

    Added initial SendBugReport Frame

--- src/JackCrashHandler/FrameCrashHandler.Designer.cs
+++ src/JackCrashHandler/FrameCrashHandler.Designer.cs
@@ -28,47 +28,46 @@
       /// </summary>
       private void InitializeComponent()
       {
-         this.label1 = new System.Windows.Forms.Label();
-         this.pictureBox1 = new System.Windows.Forms.PictureBox();
-         ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-         this.SuspendLayout();
+         label1 = new Label();
+         pictureBox1 = new PictureBox();
+         ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+         SuspendLayout();
          // 
          // label1
          // 
-         this.label1.AutoSize = true;
-         this.label1.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-         this.label1.Location = new System.Drawing.Point(74, 20);
-         this.label1.Name = "label1";
-         this.label1.Size = new System.Drawing.Size(351, 48);
-         this.label1.TabIndex = 0;
-         this.label1.Text = "Please wait while we collect information\r\nabout the crash...";
-         this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+         label1.AutoSize = true;
+         label1.Font = new Font("Calibri", 12F, FontStyle.Bold, GraphicsUnit.Point);
+         label1.Location = new Point(74, 20);
+         label1.Name = "label1";
+         label1.Size = new Size(351, 48);
+         label1.TabIndex = 0;
+         label1.Text = "Please wait while we collect information\r\nabout the crash...";
+         label1.TextAlign = ContentAlignment.MiddleCenter;
          // 
          // pictureBox1
          // 
-         this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
-         this.pictureBox1.Image = global::JackCrashHandler.Properties.Resources.Spinner;
-         this.pictureBox1.Location = new System.Drawing.Point(0, 0);
-         this.pictureBox1.Name = "pictureBox1";
-         this.pictureBox1.Size = new System.Drawing.Size(494, 253);
-         this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
-         this.pictureBox1.TabIndex = 1;
-         this.pictureBox1.TabStop = false;
+         pictureBox1.Dock = DockStyle.Fill;
+         pictureBox1.Image = Properties.Resources.Spinner;
+         pictureBox1.Location = new Point(0, 0);
+         pictureBox1.Name = "pictureBox1";
+         pictureBox1.Size = new Size(494, 253);
+         pictureBox1.SizeMode = PictureBoxSizeMode.CenterImage;
+         pictureBox1.TabIndex = 1;
+         pictureBox1.TabStop = false;
          // 
          // FrameCrashHandler
          // 
-         this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
-         this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-         this.ClientSize = new System.Drawing.Size(494, 253);
-         this.Controls.Add(this.label1);
-         this.Controls.Add(this.pictureBox1);
-         this.Name = "FrameCrashHandler";
-         this.ShowIcon = false;
-         this.Text = "Crash Handler";
-         ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-         this.ResumeLayout(false);
-         this.PerformLayout();
-
+         AutoScaleDimensions = new SizeF(8F, 20F);
+         AutoScaleMode = AutoScaleMode.Font;
+         ClientSize = new Size(494, 253);
+         Controls.Add(label1);
+         Controls.Add(pictureBox1);
+         Name = "FrameCrashHandler";
+         ShowIcon = false;
+         Text = "Crash Handler";
+         ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+         ResumeLayout(false);
+         PerformLayout();
       }
 
       #endregion
