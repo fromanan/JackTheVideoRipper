24aa4de5a91629e9a79691680159b5079f25374e
Tony Froman <33915703+fromanan@users.noreply.github.com>
2023-05-02 17:40:31 -0400

    Moved FfmpegFrame to its own class

---
+++ src/JackTheVideoRipper/models/FfmpegFrame.cs
@@ -0,0 +1,41 @@
+﻿using JackTheVideoRipper.extensions;
+
+namespace JackTheVideoRipper.models;
+
+public readonly struct FfmpegFrame
+{
+    public readonly int Frame;
+    public readonly float Fps;
+    public readonly float Q;
+    public readonly long Size;
+    public readonly TimeOnly Time;
+    public readonly float Bitrate;
+    public readonly string BitrateUnit;
+    public readonly float Speed;
+
+    public FfmpegFrame(IReadOnlyList<string> tokens)
+    {
+        int count = 0;
+        Frame = int.TryParse(GetField(tokens, ref count), out int frame) ? frame : -1;
+        Fps = float.TryParse(GetField(tokens, ref count), out float fps) ? fps : -1;
+        Q = float.TryParse(GetField(tokens, ref count), out float q) ? q : -1;
+        Size = long.TryParse(GetField(tokens, ref count).BeforeFirstLetter(), out long size) ? size * 1000 : -1;
+        Time = TimeOnly.TryParse(GetField(tokens, ref count), out TimeOnly time) ? time : default;
+
+        string bitrateField = GetField(tokens, ref count);
+        Bitrate = float.TryParse(bitrateField.BeforeFirstLetter(), out float bitrate) ? bitrate : -1;
+        BitrateUnit = bitrateField.AfterFirstLetter();
+            
+        Speed = float.TryParse(GetField(tokens, ref count), out float speed) ? speed : -1;
+    }
+    
+    private static string GetField(IReadOnlyList<string> tokens, ref int count)
+    {
+        string value = tokens[count++];
+        if (value.ContainsNumber())
+            return value.Split('=')[1];
+        if (count >= tokens.Count)
+            return string.Empty;
+        return tokens[count++];
+    }
+}
--- src/JackTheVideoRipper/models/rows/CompressProcessUpdateRow.cs
+++ src/JackTheVideoRipper/models/rows/CompressProcessUpdateRow.cs
@@ -60,16 +60,6 @@ public class CompressProcessUpdateRow : ProcessUpdateRow
         return Common.TimeString(((float) _totalFrames - frame) / fps);
     }
 
-    public static string GetField(IReadOnlyList<string> tokens, ref int count)
-    {
-        string value = tokens[count++];
-        if (value.ContainsNumber())
-            return value.Split('=')[1];
-        if (count >= tokens.Count)
-            return string.Empty;
-        return tokens[count++];
-    }
-
     protected override async Task<string> GetTitle()
     {
         return await ExifTool.GetTag(Filepath, "Title");
@@ -96,31 +86,6 @@ public class CompressProcessUpdateRow : ProcessUpdateRow
         _totalFrames = _exifData.Frames > 0 ? _exifData.Frames : await FFMPEG.GetNumberOfFrames(Filepath);
     }
 
-    private readonly struct FfmpegFrame
     {
-        public readonly int Frame;
-        public readonly float Fps;
-        public readonly float Q;
-        public readonly long Size;
-        public readonly TimeOnly Time;
-        public readonly float Bitrate;
-        public readonly string BitrateUnit;
-        public readonly float Speed;
-
-        public FfmpegFrame(IReadOnlyList<string> tokens)
-        {
-            int count = 0;
-            Frame = int.TryParse(GetField(tokens, ref count), out int frame) ? frame : -1;
-            Fps = float.TryParse(GetField(tokens, ref count), out float fps) ? fps : -1;
-            Q = float.TryParse(GetField(tokens, ref count), out float q) ? q : -1;
-            Size = long.TryParse(GetField(tokens, ref count).BeforeFirstLetter(), out long size) ? size * 1000 : -1;
-            Time = TimeOnly.TryParse(GetField(tokens, ref count), out TimeOnly time) ? time : default;
-
-            string bitrateField = GetField(tokens, ref count);
-            Bitrate = float.TryParse(bitrateField.BeforeFirstLetter(), out float bitrate) ? bitrate : -1;
-            BitrateUnit = bitrateField.AfterFirstLetter();
-            
-            Speed = float.TryParse(GetField(tokens, ref count), out float speed) ? speed : -1;
-        }
     }
 }
