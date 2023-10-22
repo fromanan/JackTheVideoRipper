b5dd0554dc72b6b7f1b761d387be55f26bf1b94e
Tony Froman <33915703+fromanan@users.noreply.github.com>
2023-10-22 11:07:42 -0400

    Create RowUpdateArgs.cs

---
+++ src/Application/models/rows/RowUpdateArgs.cs
@@ -0,0 +1,4 @@
+﻿namespace JackTheVideoRipper.models.rows;
+
+public record RowUpdateArgs(string? Status = null, string? MediaType = null, string? FileSize = null,
+    string? Progress = null, string? Speed = null, string? Eta = null, string? Url = null, string? Path = null);
