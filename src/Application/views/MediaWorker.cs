aca4f81175330c394c1ec7f6fc6d71c975aee23b
Tony Froman <33915703+fromanan@users.noreply.github.com>
2023-10-22 11:07:44 -0400

    Create MediaWorker.cs

---
+++ src/Application/views/MediaWorker.cs
@@ -0,0 +1,65 @@
+﻿using System.ComponentModel;
+using JackTheVideoRipper.models;
+using JackTheVideoRipper.models.processes;
+
+namespace JackTheVideoRipper.views;
+
+/// <summary>
+/// Container class for a Media Task
+/// This is NOT updating the external process itself, only checking the output of the Process via the
+///     ProcessBuffer (OutputStream) periodically and pushing the updates to the view via the ReportProgress method
+/// All actions are controlled by the external process (exe) which we own a handle to, our program can, however,
+///     pause/resume or cancel the external process
+/// This class is reused by the ProcessPool and may run a task multiple times
+/// </summary>
+public class MediaWorker : BackgroundWorker, IClaimable
+{
+    /// <summary>
+    /// Sleep delay between update cycles
+    /// </summary>
+    private const int _UPDATE_FREQUENCY = 1000;
+
+    /// <summary>
+    /// Used in passing the progress back to the ProcessPool since it only uses whole numbers 1-100
+    /// </summary>
+    public const float PROGRESS_PRECISION_FACTOR = 1E2f;
+    
+    /// <summary>
+    /// Threading property used when our resource is Claimed by a process, but has not started yet (or is paused)
+    /// </summary>
+    public bool Claimed { get; set; }
+
+    /// <summary>
+    /// Called prior to running, but after instantiating the class (we may do that at runtime when the containing class
+    ///     is created, if the instance is a property)
+    /// </summary>
+    public void InitializeWorker()
+    {
+        WorkerReportsProgress = true;
+        WorkerSupportsCancellation = true;
+        DoWork += DoMediaWork;
+    }
+    
+    /// <summary>
+    /// Method called when the BackgroundWorker.RunAsync() is called
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="args"></param>
+    private async void DoMediaWork(object? sender, DoWorkEventArgs args)
+    {
+        if (args.Argument is not ProcessRunner processRunner)
+            return;
+
+        while (!CancellationPending && !processRunner.Completed)
+        {
+            // TODO: Should we only trigger an update when the text changes?
+            ProcessUpdateArgs updateArgs = await processRunner.Update();
+        
+            ReportProgress((int)(processRunner.Progress * PROGRESS_PRECISION_FACTOR), updateArgs);
+
+            Thread.Sleep(_UPDATE_FREQUENCY);
+        }
+
+        Claimed = false;
+    }
+}
