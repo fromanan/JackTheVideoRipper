1539d3787dc75fd641913ad7eab52c311977ed0b
Tony Froman <33915703+fromanan@users.noreply.github.com>
2023-10-22 11:07:38 -0400

    Create ProcessUpdateArgs.cs

---
+++ src/Application/models/processes/ProcessUpdateArgs.cs
@@ -0,0 +1,10 @@
+﻿using JackTheVideoRipper.models.rows;
+
+namespace JackTheVideoRipper.models.processes;
+
+public record ProcessUpdateArgs(bool Completed, RowUpdateArgs? RowUpdateArgs = null)
+{
+    public static readonly ProcessUpdateArgs Default = new(false);
+
+    public static readonly ProcessUpdateArgs Done = new(true);
+}
