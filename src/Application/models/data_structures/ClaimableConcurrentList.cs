8e05f341f54d1706b8bee7d90ae8be05ac9ee60a
Tony Froman <33915703+fromanan@users.noreply.github.com>
2024-02-21 20:47:06 -0500

    Added ClaimableConcurrentList and UpdateBuffer data structures

---
+++ src/Application/models/data_structures/ClaimableConcurrentList.cs
@@ -0,0 +1,34 @@
+﻿using JackTheVideoRipper.interfaces;
+
+namespace JackTheVideoRipper.models.DataStructures;
+
+public class ClaimableConcurrentList<T> : ConcurrentList<T> where T : IClaimable, new()
+{
+    #region Constructors
+
+    public ClaimableConcurrentList() { }
+    
+    public ClaimableConcurrentList(int size) : base(size) { }
+
+    #endregion
+    
+    public async Task<T> Next(Func<T, bool> predicate, int tickInMilliseconds)
+    {
+        await Tasks.WaitUntil(Where(predicate).Any, tickInMilliseconds);
+        
+        T result = First(predicate);
+
+        AccessLock.EnterWriteLock();
+        
+        try
+        {
+            result.Claimed = true;
+        }
+        finally
+        {
+            AccessLock.ExitWriteLock();
+        }
+        
+        return result;
+    }
+}
---
+++ src/Application/models/data_structures/UpdateBuffer.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections.Concurrent;
+using Timer = System.Threading.Timer;
+
+namespace JackTheVideoRipper.models.DataStructures;
+
+public class UpdateBuffer<T>
+{
+    #region Data Members
+
+    private readonly ConcurrentDictionary<string, T> _table = new();
+    
+    private readonly Timer _updateTimer;
+
+    private const int _DEFAULT_UPDATE_PERIOD = 500;
+
+    #endregion
+
+    #region Properties
+    
+    public int UpdatePeriod
+    {
+        set => _updateTimer.Change(0, value);
+    }
+    
+    public bool Empty => _table.IsEmpty;
+
+    public IEnumerable<T> Data => _table.Values;
+
+    #endregion
+
+    #region Events
+
+    public event Action<IEnumerable<T>> OnBufferDispatched = delegate { };
+
+    #endregion
+
+    #region Constructor
+
+    public UpdateBuffer(int updatePeriod = _DEFAULT_UPDATE_PERIOD)
+    {
+        _updateTimer = new Timer(Dispatch, null, 0, updatePeriod);
+    }
+
+    #endregion
+
+    #region Public Methods
+
+    /// <summary>
+    /// Public facing method to queue a new task into the buffer
+    /// </summary>
+    /// <param name="key"></param>
+    /// <param name="value"></param>
+    public void Add(string key, T value)
+    {
+        _table[key] = value;
+    }
+
+    #endregion
+
+    #region Private Methods
+
+    /// <summary>
+    /// Timed event to release all pending tasks
+    /// </summary>
+    /// <param name="state"></param>
+    private void Dispatch(object? state)
+    {
+        if (Empty)
+            return;
+        OnBufferDispatched.Invoke(Data.ToArray());
+        _table.Clear();
+    }
+
+    #endregion
+}
