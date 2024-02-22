bf0517ffdebf7b2f6d1b4a18f1271826cd00a8ff
Tony Froman <33915703+fromanan@users.noreply.github.com>
2024-02-21 20:45:24 -0500

    Moved DragEventArgsExtension to FormExtensions folder

---
+++ src/Application/extensions/FormsExtensions/DragEventArgsExtensions.cs
@@ -0,0 +1,29 @@
+﻿namespace JackTheVideoRipper.extensions;
+
+public static class DragEventArgsExtensions
+{
+    public static bool IsText(this DragEventArgs args)
+    {
+        return args.Data?.GetDataPresent(DataFormats.Text) ?? false;
+    }
+
+    public static object? AsText(this DragEventArgs args)
+    {
+        return args.Data?.GetData(DataFormats.Text);
+    }
+    
+    public static bool IsFile(this DragEventArgs args)
+    {
+        return args.Data?.GetDataPresent(DataFormats.FileDrop) ?? false;
+    }
+
+    public static object? AsFile(this DragEventArgs args)
+    {
+        return args.Data?.GetData(DataFormats.FileDrop);
+    }
+    
+    public static bool IsValidDroppable(this DragEventArgs args)
+    {
+        return args.IsText() || args.IsFile();
+    }
+}
