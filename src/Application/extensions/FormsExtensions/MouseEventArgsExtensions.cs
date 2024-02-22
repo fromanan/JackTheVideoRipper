c8adbf0d04ccd74a9fa6b1a72db5ab2739fb9885
Tony Froman <33915703+fromanan@users.noreply.github.com>
2024-02-21 20:44:19 -0500

    Moved MouseEventArgsExtensions to FormExtensions folder

---
+++ src/Application/extensions/FormsExtensions/MouseEventArgsExtensions.cs
@@ -0,0 +1,19 @@
+﻿namespace JackTheVideoRipper.extensions;
+
+public static class MouseEventArgsExtensions
+{
+    public static bool IsRightClick(this MouseEventArgs args)
+    {
+        return args.Button is MouseButtons.Right;
+    }
+    
+    public static bool IsLeftClick(this MouseEventArgs args)
+    {
+        return args.Button is MouseButtons.Left;
+    }
+    
+    public static bool IsMiddleClick(this MouseEventArgs args)
+    {
+        return args.Button is MouseButtons.Middle;
+    }
+}
