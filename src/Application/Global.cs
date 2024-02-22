892993d6e34da641a2ea0c1ebad15d4a30be3033
Tony Froman <33915703+fromanan@users.noreply.github.com>
2024-02-21 20:59:09 -0500

    Added Global.cs for Constants

---
+++ src/Application/Global.cs
@@ -0,0 +1,34 @@
+﻿using System.Diagnostics;
+
+namespace JackTheVideoRipper;
+
+internal static class Global
+{
+    internal static class Fonts
+    {
+        public const string FAMILY_NAME = "Segoe UI Semibold";
+
+        public static readonly Font Header = new(FAMILY_NAME, 9.5f);
+    }
+
+    internal static class Configurations
+    {
+        public static bool VerboseDebugMode => Debugger.IsAttached;
+
+        public static bool CatchExceptions = true;
+
+        public const int VIEW_UPDATE_TIMEOUT = 500;
+        
+        public const int WORKER_QUEUE_CHECK_FREQUENCY = 500;
+
+        public const int WORKER_UPDATE_FREQUENCY = 100;
+        
+        public const int NOTIFICATION_CLEAR_TIME = 5000;
+        
+        public const int DEFAULT_TASK_TICK = 300;
+
+        public const int PROCESS_POOL_UPDATE_FREQUENCY = 200;
+        
+        public const int WEB_REQUEST_TIMEOUT_PERIOD = 60000;
+    }
+}
