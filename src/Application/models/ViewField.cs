61d4c1b4b731484f70a23b39c5bce0af876eef30
Tony Froman <33915703+fromanan@users.noreply.github.com>
2024-02-25 21:00:54 -0500

    Simplified ViewField multi-flag definitions

--- src/Application/models/ViewField.cs
+++ src/Application/models/ViewField.cs
@@ -13,7 +13,7 @@ public enum ViewField
     MediaType   = 1<<6,
     Url         = 1<<7,
     Path        = 1<<8,
-    Static      = (1<<5) | (1<<6) | (1<<7) | (1<<8),
-    Dynamic     = (1<<0) | (1<<1) | (1<<2) | (1<<3) | (1<<4),
-    All         = (1<<0) | (1<<1) | (1<<2) | (1<<3) | (1<<4) | (1<<5) | (1<<6) | (1<<7) | (1<<8),
+    Static      = Title | MediaType | Url | Path,
+    Dynamic     = Status | Size | Progress | Speed | Eta,
+    All         = Status | Size | Progress | Speed | Eta | Title | MediaType | Url | Path,
 }
