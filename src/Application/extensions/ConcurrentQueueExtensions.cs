5c894f8fef0ffa0458c5ceeae5d058f6f25287a3
Tony Froman <33915703+fromanan@users.noreply.github.com>
2024-05-13 16:34:40 -0400

    Create ConcurrentQueueExtensions.cs

---
+++ src/Application/extensions/ConcurrentQueueExtensions.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Concurrent;
+
+namespace JackTheVideoRipper.extensions;
+
+public static class ConcurrentQueueExtensions
+{
+    public static void Extend<T>(this ConcurrentQueue<T> queue, IEnumerable<T> enumerable)
+    {
+        enumerable.ForEach(queue.Enqueue);
+    }
+}
